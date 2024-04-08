153091ee23feddec2721a4f6e5392eed14d4754a
Сергей Куклин <sergeymulti@gmail.com>
2024-04-08 21:27:27 +0300

    add_bd_half

--- tg_bot.py
+++ tg_bot.py
@@ -1,11 +1,14 @@
 import logging
 from telegram.ext import Application, MessageHandler, filters, CommandHandler
+from data import db_session
+from data.users import User
 
 BOT_TOKEN='7003047434:AAFLOJSvtnsxSb2eosLtMxKa3xzKY002uME'
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG
 )
 logger = logging.getLogger(__name__)
+db_session.global_init("db/travel.db")
 
 
 async def start(update, context):
@@ -20,11 +23,16 @@ async def answerer(update, context):
     await update.message.reply_text('Не пиши сюда')
 
 
+async def login(update, context):
+    await update.message.reply_text('Введите свой логин и пароль через пробел')
+    db_sess = db_session.create_session()
+
+
 def main():
     application = Application.builder().token(BOT_TOKEN).build()
     application.add_handler(CommandHandler('start', start))
     application.add_handler(CommandHandler('help', helper))
-    application.add_handler(MessageHandler(filters.TEXT, answerer))
+    application.add_handler(CommandHandler('login', login))
     application.run_polling()
 
 
