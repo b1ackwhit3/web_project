7fe669efd5e8ae3aa2e5f13ac4a5b39ce9bd5053
Сергей Куклин <sergeymulti@gmail.com>
2024-04-17 02:09:40 +0300

    polirovka

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from data.users import User
 import datetime
 from flask_login import LoginManager, login_user, logout_user, login_required
 from forms.login import LoginForm
+from forms.reg import RegisterForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -24,29 +25,6 @@ def load_user(user_id):
     return db_sess.query(User).get(user_id)
 
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        db_sess = db_session.create_session()
-        user = db_sess.query(User).filter(User.name == form.name.data).first()
-        if user and user.check_password(form.password.data):
-            login_user(user, remember=form.remember_me.data)
-            return redirect("/")
-        return render_template('login.html',
-                               message="Неправильный логин или парол",
-                               form=form)
-    return render_template('login.html', title='Авторизация', form=form)
-
-
-@app.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    return redirect("/")
-
-
-"""
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm()
@@ -61,7 +39,6 @@ def register():
         user = User(
             name=form.name.data,
             email=form.email.data,
-            about=form.about.data
         )
         user.set_password(form.password.data)
         db_sess.add(user)
@@ -71,31 +48,12 @@ def register():
                            form=form)
 
 
-@app.route('/cookie')
-def cookie_test():
-    visits_count = int(request.cookies.get('visits_count', 0))
-    if visits_count:
-        ans = make_response(f'Вы пришли на страницу {visits_count + 1} раз')
-        ans.set_cookie('visits_count', str(visits_count + 1), max_age=60 * 60 * 24)
-    else:
-        ans = make_response(f'Вы пришли на страницу первый раз за сутки')
-        ans.set_cookie('visits_count', '1', max_age=60 * 60 * 24)
-    return ans
-
-
-@app.route('/session')
-def session_test():
-    visits_count = session.get('visits_count', 0)
-    session['visits_count'] = visits_count + 1
-    return make_response(f'Вы пришли на страницу {visits_count + 1} раз')
-
-
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
         db_sess = db_session.create_session()
-        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        user = db_sess.query(User).filter(User.name == form.name.data).first()
         if user and user.check_password(form.password.data):
             login_user(user, remember=form.remember_me.data)
             return redirect('/')
@@ -109,8 +67,6 @@ def login():
 def logout():
     logout_user()
     return redirect('/')
-
-"""
 
 
 def main():
