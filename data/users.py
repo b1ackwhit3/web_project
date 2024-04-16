11ac4d964fbb8ee214a3b279ed0cae9f7828bc43
Сергей Куклин <sergeymulti@gmail.com>
2024-04-17 00:49:14 +0300

    login_added

--- data/users.py
+++ data/users.py
@@ -1,9 +1,11 @@
 import sqlalchemy
+from flask_login import UserMixin
 from .db_session import SqlAlchemyBase, create_session
 from sqlalchemy import orm
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
-class User(SqlAlchemyBase):
+class User(SqlAlchemyBase, UserMixin):
     def __repr__(self):
         db_sess = create_session()
         for user in db_sess.query(User).all():
@@ -16,3 +18,9 @@ class User(SqlAlchemyBase):
     email = sqlalchemy.Column(sqlalchemy.String, index=True, unique=True, nullable=True)
     password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     review = orm.relationship("Review", back_populates='user')
+
+    def set_password(self, password):
+        self.password = password
+
+    def check_password(self, password):
+        return self.password == password
