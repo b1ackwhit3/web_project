323344691df3710a19ed616f27ef8ba433cdb8f6
osokinaelya <151373765+osolinaelya@users.noreply.github.com>
2024-04-22 20:03:36 +0300

    Add files via upload

---
+++ main1.py
@@ -0,0 +1,67 @@
+from flask import Flask, render_template, redirect
+from data import db_session
+from data.users import User
+from forms.reg import RegisterForm
+from flask_login import login_user, login_required, logout_user
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'traveler'
+
+
+@app.route('/')
+def index():
+    pass
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form, message='Пароли не совпадают повторить попытку')
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form, message='Пользователь уже существует')
+        user = User(
+            name=form.name.data,
+            email=form.email.data,
+            about=form.about.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/')
+    return render_template('register.html', title='Регистрация',
+                           form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect('/')
+        return render_template('login.html', title='Авторизация',
+                               message='Неверный логин или пароль', form=form)
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect('/')
+
+
+def main():
+    db_session.global_init('db/travel.db')
+    app.run()
+
+
+if __name__ == '__main__':
+    main()
